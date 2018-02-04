e1f481c3316d2ea258fef314545317f597dbdfca
David Kim <kim.david.j@gmail.com>
2018-02-03 19:24:20 -0800

    Replaced absolute path ==> ${DIR}

--- rosterbot.sh
+++ rosterbot.sh
@@ -2,9 +2,12 @@
 # rosterbot ~ main
 # Copyright (c) 2017 David Kim
 # This program is licensed under the "MIT License".
+# Date of inception: 11/21/17
 
-# LOG_FILE_1=/u/dkim/sandbox/rosterbot/log.stdout        # Redirect file descriptors 1 and 2 to log.out
-# LOG_FILE_2=/u/dkim/sandbox/rosterbot/log.stderr
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"     # Path to rosterbot.
+
+# LOG_FILE_1=${DIR}/log.stdout        # Redirect file descriptors 1 and 2 to log.out
+# LOG_FILE_2=${DIR}/log.stderr
 # exec > >(tee -a ${LOG_FILE_1} )
 # exec 2> >(tee -a ${LOG_FILE_2} >&2)
 
@@ -35,8 +38,8 @@ mkfifo ${BOT_NICK}.io
 tail -f ${BOT_NICK}.io | openssl s_client -connect irc.cat.pdx.edu:6697 | while true ; do
 
     # # If log.out is empty, reset logging.  (cron job empties log.out after backup)
-    # LOG_FILE_1=/u/dkim/sandbox/rosterbot/log.stdout
-    # LOG_FILE_2=/u/dkim/sandbox/rosterbot/log.stderr
+    # LOG_FILE_1=${DIR}/log.stdout
+    # LOG_FILE_2=${DIR}/log.stderr
     # if [ ! -s ${LOG_FILE_1} ] && [ ! -s ${LOG_FILE_2} ] ; then
     #     exec > >(tee -a ${LOG_FILE_1} )
     #     exec 2> >(tee -a ${LOG_FILE_2} >&2)
